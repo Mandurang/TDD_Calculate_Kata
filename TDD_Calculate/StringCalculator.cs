b47b9f7a4914d84729ecb594b0681817b84b7e4c
Mandurang <56937341+Mandurang@users.noreply.github.com>
2024-02-12 21:45:59 +0000

    Add project files.

---
+++ TDD_Calculate/StringCalculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Calculate
+{
+    public delegate void AddOccured();
+    class StringCalculator
+    {
+        private int Count;
+        public event Action<string, int> AddOccured;
+
+        public StringCalculator()
+        {
+            Count = 0;
+        }
+
+        public int Add(string numbers)
+        {
+            Count++;
+            int result = 0;
+            OnAddOccurred("Method was called", Count);
+
+            char[] separators = { '\n' , ',', ';'};
+
+             if (numbers == string.Empty || numbers == "")
+             {
+                 return 0;
+             }
+
+             var arrayNumbers = numbers.Split(separators);
+
+             foreach (var number in arrayNumbers)
+             {
+                 Int32.TryParse(number, out int value);
+                 if (value < 0)
+                     throw new ArgumentException($"Negatives not allowed {value}");
+                 result += value;
+             }
+             return result;
+        }
+
+        public int GetCalledCount()
+        {
+            return Count;
+        }
+
+        protected virtual void OnAddOccurred(string message, int value)
+        {
+            AddOccured?.Invoke(message, value);
+        }
+    }
+}
---
+++ TDD_Calculate/UnitTests/CalculatorTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TDD_Calculate.UnitTests
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void SumWithEmptyString()
+        {
+            //ARRAGE
+            StringCalculator calculator = new StringCalculator();
+            string emptyInput = "";
+            int testResult = 0;
+            //ACT 
+            var expectedResult = calculator.Add(emptyInput);
+            //ASSERT
+            Assert.Equal(expectedResult, testResult);
+        }
+
+        [Fact]
+        public void SumWithOneValue()
+        {
+            //ARRAGE
+            StringCalculator calculator = new StringCalculator();
+            string oneValueInput = "2";
+            int testResult = 2;
+            //ACT 
+            var expectedResult = calculator.Add(oneValueInput);
+            //ASSERT
+            Assert.Equal(expectedResult, testResult);
+        }
+
+        [Fact]
+        public void SumWithTwoValue()
+        {
+            //ARRAGE
+            StringCalculator calculator = new StringCalculator();
+            string twoValueInput = "2,5";
+            int testResult = 7;
+            //ACT 
+            var expectedResult = calculator.Add(twoValueInput);
+            //ASSERT
+            Assert.Equal(expectedResult, testResult);
+        }
+
+        [Fact]
+        public void SumWithSeparators()
+        {
+            //ARRAGE
+            StringCalculator calculator = new StringCalculator();
+            string separatorsInput = "1\n2,3";
+            int testResult = 6;
+            //ACT 
+            var expectedResult = calculator.Add(separatorsInput);
+            //ASSERT
+            Assert.Equal(expectedResult, testResult);
+        }
+
+        [Fact]
+        public void SumWithDelimiters()
+        {
+            //ARRAGE
+            StringCalculator calculator = new StringCalculator();
+            string separatorsInput = "//;\n1;2";
+            int testResult = 3;
+            //ACT 
+            var expectedResult = calculator.Add(separatorsInput);
+            //ASSERT
+            Assert.Equal(expectedResult, testResult);
+        }
+
+        [Fact]
+        public void SumThrowsArgumentException()
+        {
+            //ARRAGE
+            StringCalculator calculator = new StringCalculator();
+            string separatorsInput = "-1,-2;-3";
+            //ACT&ASSERT
+            Assert.Throws<ArgumentException>(() => calculator.Add(separatorsInput));
+        }
+
+        [Fact]
+        public void GetSumCount()
+        {
+            //ARRAGE
+            StringCalculator calculator = new StringCalculator();
+            string separatorsInput = "1\n2,3";
+            calculator.Add(separatorsInput);
+            int testResult = 1;
+            //ACT 
+            var expectedResult = calculator.GetCalledCount();
+            //ASSERT
+            Assert.Equal(expectedResult, testResult);
+        }
+    }
+}
